c117a3ea5ea8872458d17f618f2134f19b7d8f2a
Jeremy Doucet <jeremy.doucet93@gmail.com>
2015-04-16 11:07:58 -0700

    Initial Commit

---
+++ JavaScriptDay04/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JavaScriptDay04")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JavaScriptDay04")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05deb1ef-b1ab-48c6-a6b3-dca0b0b7de97")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JavaScriptDay04/Script.js
@@ -0,0 +1,46 @@
+﻿var firebaseUrl = "https://ccamplesson.firebaseio.com/"
+var Contact = function (name, email, phone) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+}
+var contacts = [];
+
+var addContact = function () {
+    //var name = document.getElementById('inputName').value;
+    var name = $('#inputName').val();
+    var email = $('#inputEmail').val();
+    var phone = $('#inputPhone').val();
+    var myContact = new Contact(name, email, phone);
+
+    postContact(myContact);
+
+    //Clears out the inputs
+    $('#inputName').val(''); $('#inputEmail').val(''); $('#inputPhone').val('');
+}
+var PrintContacts = function () {
+    //document.getElementById('DisplayContactsHere').innerHTML = '';
+    $('#DisplayContactsHere').html('');
+    var elemString = '';
+    for (var i = 0; i < contacts.length; i++) {
+
+    }
+    $('#DisplayContactsHere').html(elemString);
+}
+
+var postContact = function (data) {
+    var request = new XMLHttpRequest();
+    request.open('POST', firebaseUrl + '.json', true);
+    request.onload = function () {
+        if (this.status < 400 && this.status >= 200) {
+            var response = JSON.parse(this.response);
+            data.key = response.name;
+            contacts.push(data);
+            PrintContacts();
+        }
+        else {
+            console.log(this.response);
+        }
+    }
+    request.send(JSON.stringify(data));
+}
